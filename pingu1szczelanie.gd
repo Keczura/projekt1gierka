5ba05b5f77dbf32a0cd3f63933b61a1be5e141d2
BillyStrong <77360226+BillyStrong@users.noreply.github.com>
2025-12-11 23:27:22 +0100

    strzelanie

--- pingu1szczelanie.gd
+++ pingu1szczelanie.gd
@@ -1,7 +1,7 @@
 extends Marker2D
 
 @onready var projectile = preload("res://plociski.tscn")
-
+@export var celownik: Marker2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -11,3 +11,4 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		var projectile_temp = projectile.instantiate()
 		owner.add_child(projectile_temp)
+		projectile_temp.transform = celownik.global_transform
--- plociski.gd
+++ plociski.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-@export var speed : int = 200
+@export var speed : int = 150
 
 
 func _physics_process(delta):
-	position =+ speed * transform.x * delta
+	position += speed * transform.x * delta
  	
 	
 	
