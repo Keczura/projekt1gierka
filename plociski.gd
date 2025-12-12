9378dabd2e79a048a014ae9952a308b39113207f
BillyStrong <77360226+BillyStrong@users.noreply.github.com>
2025-12-12 22:22:51 +0100

    znikanie pocisków
    pociski znikają po trafieniu wroga i zniknięciu z pola widzenia kamery

--- plociski.gd
+++ plociski.gd
@@ -5,7 +5,16 @@ extends Area2D
 
 func _physics_process(delta):
 	position += speed * transform.x * delta
- 	
 	
-	
-	
+
+
+
+
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	queue_free()
